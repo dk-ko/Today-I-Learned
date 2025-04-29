b10256c0462b7381b6ba58994567599317f9e4b5
jade ko <dev.dkko@gmail.com>
2025-04-29 09:55:13 +0900

    react | feat: 감정일기장 Home 페이지 기능 구현

--- onebite-react/section12/src/App.jsx
+++ onebite-react/section12/src/App.jsx
@@ -11,16 +11,28 @@ import Notfound from './pages/Notfount'
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2025-04-29").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2025-04-28").getTime(),
     emotionId: 2,
     content: "2번 일기 내용",
   },
+  {
+    id: 3,
+    createdDate: new Date("2025-03-29").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  },
+  {
+    id: 4,
+    createdDate: new Date("2025-03-28").getTime(),
+    emotionId: 4,
+    content: "4번 일기 내용",
+  }
 ];
 
 function reducer(state, action) {
@@ -36,13 +48,13 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
-  const isRef = useRef(3);
+  const isRef = useRef(5);
   
   const onCreate = (createdDate, emotionId, content) => {
     dispatch({
